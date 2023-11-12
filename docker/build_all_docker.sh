51209809d4aaee9795a298fb5fb58d75cd1a382f
Fabrice Derepas <fabrice@cn17.org>
2023-11-12 22:27:37 +0000

    added automatic build script

--- docker/build_all_docker.sh
+++ docker/build_all_docker.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-for i in `ls -F | grep / | grep -v ^erlang/$ | grep -v ^apl/$ | grep -v ^bqn/$ | grep -v ^dc/$ | grep -v haskell | grep -v clojure | grep -v kotlin | grep -v ^k/$ | grep -v ^r/$   `; do
+for i in `ls -F | grep / `; do
     cd $i
-    echo Building docker for $i
-    make > /dev/null 2> /dev/null
-    if [ $? -eq 0 ]; then
+    j=`echo $i | tr -d '/'`
+    c=$(docker images | grep $j | grep `date +"%Y%m%d"` | wc -l)
+    if [ $c -eq 1 ]; then
+        # an image made today is already here, just test
+        echo testing $j
         cd test
         make > /dev/null 2> /dev/null
         if [ $? -eq 0 ]; then
@@ -11,9 +13,23 @@ for i in `ls -F | grep / | grep -v ^erlang/$ | grep -v ^apl/$ | grep -v ^bqn/$ |
         else
             printf \\033[31mtest_\KO\\033[0m"\n"
         fi
-        cd ../.. 
+        cd ../..         
     else
-        printf \\033[31mKO\\033[0m"\n"
-        cd ..
+        # build and test
+        echo Building docker for $j
+        make > /dev/null 2> /dev/null
+        if [ $? -eq 0 ]; then
+            cd test
+            make > /dev/null 2> /dev/null
+            if [ $? -eq 0 ]; then
+                printf \\033[32mOK\\033[0m"\n"
+            else
+                printf \\033[31mtest_\KO\\033[0m"\n"
+            fi
+            cd ../.. 
+        else
+            printf \\033[31mKO\\033[0m"\n"
+            cd ..
+        fi
     fi
 done
