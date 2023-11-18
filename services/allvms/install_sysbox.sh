b4b3c03ed751df9712ab8d0de778f982027d9b81
Fabrice Derepas <fabrice@derepas.com>
2023-11-18 11:29:07 +0100

    sysbox install script enhancenent

--- services/allvms/install_sysbox.sh
+++ services/allvms/install_sysbox.sh
@@ -4,7 +4,9 @@ set -x
 pkgName=sysbox-ce_0.6.2-0.linux_amd64.deb
 wget https://downloads.nestybox.com/sysbox/releases/v0.6.2/$pkgName
 docker rm $(docker ps -a -q) -f || echo "no docker container running."
-sudo apt-get install jq -y
+if [ `which jq | wc -l` -eq 0 ]; then
+    sudo apt-get install jq -y
+fi
 sudo apt-get install ./$pkgName -y
 rm $pkgName
 sudo systemctl status sysbox -n20
