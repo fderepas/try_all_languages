f06aff103e54934b5283afe086a3de56047e95d4
Fabrice Derepas <fabrice@cn17.org>
2023-11-23 20:57:27 +0000

    added fortran

---
+++ docker/fortran/launch.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# this script is to be run from a docker image
+countNbTest=$1
+cd /mnt/in
+set +e # support errors
+#compile the code
+gfortran -Wall -o /home/tal/a.out prog.f90 \
+    > /dev/null \
+    2> /mnt/out/err0.txt \
+    || exit 1
+# run the code
+for testcount in `seq 0 1 $countNbTest`
+do
+    timeout -s 9 10 /home/tal/a.out  \
+            < /mnt/in/input$testcount.txt \
+            > /mnt/out/out$testcount.txt  \
+            2> /mnt/out/err$testcount.txt
+    echo $? > /mnt/out/errcode$testcount.txt
+done
