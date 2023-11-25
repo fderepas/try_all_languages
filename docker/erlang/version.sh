c42c3cc5d4632b88a0b83e0bb6914b1635308ef6
Fabrice Derepas <fabrice@cn17.org>
2023-11-25 19:37:19 +0000

    fixed version

--- docker/erlang/version.sh
+++ docker/erlang/version.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # arguments after the entry point are given after image name
-docker run --rm --entrypoint /usr/bin/elixir tal-$(basename `pwd`):latest --version 2> /dev/null | tail -n 1
+docker run --rm --entrypoint /home/tal/erlang/bin/erl tal-$(basename `pwd`):latest -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"])), io:fwrite(Version), halt().' -noshell 2> /dev/null
+
+#/usr/bin/elixir tal-$(basename `pwd`):latest --version 2> /dev/null | tail -n 1
