d54c9b17fe012f0b8e7828e181e11a246fe51a3b
Fabrice Derepas <fabrice@cn17.org>
2023-12-02 21:55:56 +0000

    script to push docker images to dockerhub

---
+++ docker/docker_hub_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for i in `ls -F | grep / | tr -d /`; do
+    c=`docker images | grep "^tal-$i " | wc -l`
+    if (( c > 1 )); then
+	echo $i
+	tag_to_keep=`docker images | grep "^tal-$i " | grep latest | tr -s ' ' | cut -d ' ' -f 3`
+        imageData=`docker images | grep "$tag_to_keep" | grep -v latest | tr -s ' ' | cut -d ' ' -f 2`
+        docker tag tal-$i:$imageData fderepas/tal-$i:$imageData
+        docker tag tal-$i:$imageData fderepas/tal-$i:latest
+        docker push fderepas/tal-$i:$imageData
+        docker push fderepas/tal-$i:latest
+    fi
+done
