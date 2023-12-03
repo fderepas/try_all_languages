0bf7901f42d17c8227235049eac134fc807a91c0
Fabrice Derepas <fabrice@cn17.org>
2023-12-03 16:42:17 +0000

    update

--- docker/compute_time.sh
+++ docker/compute_time.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd `dirname $0`
 dataFile=execution_time.json
 echo "{" >  $dataFile
 firstTime=1
@@ -11,11 +11,11 @@ for i in `docker images --format "{{.Repository}} {{.Tag}}" | grep ^tal- | grep 
     fi
     cd $j/test
     a=$(/usr/bin/time  --format "%e"  make 2>&1 >/dev/null | tail -n 1)
-    b=$(/usr/bin/time  --format "%e"  make 2>&1 >/dev/null | tail -n 1)
-    c=$(/usr/bin/time  --format "%e"  make 2>&1 >/dev/null | tail -n 1)
-    d=$(/usr/bin/time  --format "%e"  make 2>&1 >/dev/null | tail -n 1)
+    for i in `seq 1 10`; do
+        a+=", "$(/usr/bin/time  --format "%e"  make 2>&1 >/dev/null | tail -n 1)
+    done
     cd ../..
-    echo -e "\"$j\": [$a, $b, $c, $d]" >> $dataFile
+    echo -e "\"$j\": [$a]" >> $dataFile
     firstTime=0
 done
 echo "}" >>  $dataFile
