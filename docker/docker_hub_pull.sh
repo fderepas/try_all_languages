ace14ddcb88bb45c272839b1f568d513ac34a067
Fabrice Derepas <fabrice@derepas.com>
2023-12-04 23:33:15 +0000

    delete old images in pull script

--- docker/docker_hub_pull.sh
+++ docker/docker_hub_pull.sh
@@ -2,7 +2,14 @@
 cd `dirname $0`
 for i in `ls -F | grep / | tr -d /`; do
     c=`docker images | grep "^tal-$i " | wc -l`
-    echo $i
+    echo '**********************'
+    echo pulling $i
     docker pull fderepas/tal-$i:latest
     docker tag fderepas/tal-$i:latest tal-$i:latest
+    # now delete old image if any
+    tag_to_keep=`docker images | grep "^tal-$i " | grep latest | tr -s ' ' | cut -d ' ' -f 3`
+    for j in `docker images | grep "tal-$i " | grep -v $tag_to_keep | tr -s ' ' | cut -d ' ' -f 3`; do
+	echo deleting $j
+	docker rmi -f $j
+    done
 done
