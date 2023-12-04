5f2e9ab0a56fcf2e16819c8348061b1c293d0a9c
Fabrice Derepas <fabrice@derepas.com>
2023-12-04 21:35:46 +0000

    added local image tag

--- docker/docker_hub_pull.sh
+++ docker/docker_hub_pull.sh
@@ -4,4 +4,5 @@ for i in `ls -F | grep / | tr -d /`; do
     c=`docker images | grep "^tal-$i " | wc -l`
     echo $i
     docker pull fderepas/tal-$i:latest
+    docker tag fderepas/tal-$i:latest tal-$i:latest
 done
