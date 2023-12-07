725e5cd5f006c92a177d0f815bf9b452c5bead5d
Fabrice Derepas <fabrice@cn17.org>
2023-12-07 22:30:40 +0000

    fixed julia example

--- docker/docker_hub_push.sh
+++ docker/docker_hub_push.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 cd `dirname $0`
-for i in `ls -F | grep / | tr -d /`; do
-    c=`docker images | grep "^tal-$i " | wc -l`
-    if (( c > 1 )); then
-	echo '**************************'
+if [ "$#" -eq 0 ]; then
+    for i in `ls -F | grep / | tr -d /`; do
+        echo '**************************'
         echo $i
-	echo '**************************'
-	tag_to_keep=`docker images | grep "^tal-$i " | grep latest | tr -s ' ' | cut -d ' ' -f 3`
+        echo '**************************'
         docker tag tal-$i:latest fderepas/tal-$i:latest
         docker push fderepas/tal-$i:latest
-    fi
-done
+    done
+fi
+if [ "$#" -eq 1 ]; then
+    i=$1
+    docker tag tal-$i:latest fderepas/tal-$i:latest
+    docker push fderepas/tal-$i:latest
+fi
