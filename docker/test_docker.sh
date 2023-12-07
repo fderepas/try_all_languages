bce3759ef2475e258f2c3258259f5f540eb32d72
Fabrice Derepas <fabrice@cn17.org>
2023-12-07 06:39:40 +0000

    added generic docker test script

---
+++ docker/test_docker.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+set -x
+if [ "$#" -ne 1 ]; then
+    echo "indicate the name of the language to test." 1>&2
+    exit 1
+fi
+cd `dirname $0`
+lang=$1
+#lang=`pwd  | sed -e 's/\(.*\)\/[^\/]*$/\1/' | sed -e 's/.*\/\([^\/]*\)$/\1/'`
+sourcefile=prog.`bash ../rest_api/rest_api_server/public/get_ext.sh $lang`
+bash ../rest_api/rest_api_server/public/get_code.sh $lang > $lang/test/$sourcefile
+cd $lang/test/
+r1=()
+r2=()
+r3=()
+for testcount in `seq 0 1 9`
+do
+    r1[$testcount]=$RANDOM
+    r2[$testcount]=$RANDOM
+    r3[$testcount]=$RANDOM
+    echo ${r1[$testcount]} > input$testcount.txt
+    echo ${r2[$testcount]} >> input$testcount.txt
+    echo ${r3[$testcount]} >> input$testcount.txt
+done
+docker run \
+       --mount type=bind,source=`pwd`,target=/mnt/in,ro \
+       --mount type=bind,source=`pwd`,target=/mnt/out \
+       --network none \
+       --name tal-$lang-$$ --rm \
+       tal-$lang:latest /bin/sh /home/tal/launch.sh 9
+for testcount in `seq 0 1 9`
+do
+    errc=`cat errcode$testcount.txt`
+    if [ "$errc" -ne "0" ]; then
+	printf "\033[31mwrong error code\033[0m";
+	exit 1;
+    fi
+    diff out$testcount.txt input$testcount.txt || errc=1
+    if [ "$errc" -ne "0" ]; then
+	set +x 
+	printf "\033[31munexpected output in out$testcount.txt\033[0m";
+	echo
+	exit 1;
+    fi
+done
+
