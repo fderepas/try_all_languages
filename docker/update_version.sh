7bb1e3be8ed074a5952d69e8c867fdba3733b36f
Fabrice Derepas <fabrice.derepas@polytechnique.org>
2024-07-09 22:04:19 +0200

    escaped double quote

--- docker/update_version.sh
+++ docker/update_version.sh
@@ -3,10 +3,9 @@ cd `dirname $0`
 echo "{" > version.json
 for i in `ls -F | grep / | tr -d '/'`; do
     cd $i
-    v=`bash version.sh 2>&1 || echo error`
-    w=`echo "$v" | tr -d '\r'`
+    v=`bash version.sh 2>&1 |  sed -e 's/\r//'|  sed -e 's/"/\\\\"/g'`
     cd ..
-    echo "\"$i\":\"$w\"," >> version.json
+    echo "\"$i\":\"$v\"," >> version.json
 done
 echo "}" >> version.json
 
