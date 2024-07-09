8b07a5717fc8c07b6cc42386ac617a9cc623f33f
Fabrice Derepas <fabrice.derepas@polytechnique.org>
2024-07-09 21:53:12 +0200

    updated pip version

--- docker/vyxal/version.sh
+++ docker/vyxal/version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # arguments after the entry point are given after image name
-docker run --rm --entrypoint /usr/bin/pip tal-vyxal:latest show vyxal | grep ^Version: | sed -e 's/Version://' | tr -d ' '
+docker run --rm --entrypoint /home/tal/venv/bin/pip3 tal-vyxal:latest show vyxal | grep ^Version: | sed -e 's/Version://' | tr -d ' '
