a36ca9469997116286d1fede62586a289c1a7a85
Fabrice Derepas <fabrice.derepas@polytechnique.org>
2024-07-12 23:09:20 +0200

    updated script to get version

--- docker/dart/version.sh
+++ docker/dart/version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # arguments after the entry point are given after image name
-docker run --rm --entrypoint /usr/bin/gcc tal-$(basename `pwd`):latest --version | head -n 1
+docker run --rm --entrypoint /usr/bin/dart tal-$(basename `pwd`):latest --version | head -n 1
