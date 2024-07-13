2c987256f93fec4ecaaa4a4ec3ee8c1e82c8ad82
Fabrice Derepas <fabrice.derepas@polytechnique.org>
2024-07-13 20:37:37 +0200

    used precompiled version of ghc

--- docker/haskell/launch.sh
+++ docker/haskell/launch.sh
@@ -4,7 +4,7 @@ countNbTest=$1
 set +e # support errors
 cd /home/tal
 cp /mnt/in/prog.hs .
-./ghc/bin/ghc prog.hs  > /dev/null 2> /mnt/out/err0.txt || exit 1
+/usr/bin/ghc prog.hs  > /dev/null 2> /mnt/out/err0.txt || exit 1
 cd /mnt
 for testcount in `seq 0 1 $countNbTest`
 do
