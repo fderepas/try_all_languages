483513c87cc98207b53702c1318da0f7ff3b25d6
Fabrice Derepas <fabrice.derepas@polytechnique.org>
2024-08-08 14:24:01 +0000

    added capability to read argv in dart

--- docker/dart/launch.sh
+++ docker/dart/launch.sh
@@ -2,11 +2,28 @@
 # this script is to be run from a docker image
 countNbTest=$1
 cd /mnt/in
+# create an empty file
+touch /home/tal/empty_file
 # run the code
 for testcount in `seq 0 1 $countNbTest`
 do
-    dart prog.dart                       \
-           < /mnt/in/input$testcount.txt \
+    stdinData=/home/tal/empty_file
+    if [ -e /mnt/in/input$testcount.txt ]; then
+        stdinData=/mnt/in/input$testcount.txt
+    fi
+    argc=0
+    if [ -e  /mnt/in/argc_${testcount}.txt ]; then
+        argc=`cat /mnt/in/argc_${testcount}.txt`
+    fi
+    # call program through timeout and execve_wrapper
+    # argv in /in/argv_${testcount}_*
+    # stdin in /mnt/in/input$testcount.txt
+    timeout -s 9 10                      \
+      /home/tal/execve_wrapper           \
+         /mnt/in/argv_${testcount}_      \
+         $argc                           \
+         /usr/bin/dart prog.dart         \
+           < $stdinData                  \
            > /mnt/out/out$testcount.txt  \
            2> /mnt/out/err$testcount.txt
     echo $? > /mnt/out/errcode$testcount.txt
