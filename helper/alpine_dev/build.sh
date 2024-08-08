69c6084d18202776bed593f51317f080bea33858
Fabrice Derepas <fabrice.derepas@polytechnique.org>
2024-08-08 13:37:28 +0000

    took into account the change of location for execve_wrapper.c

--- helper/alpine_dev/build.sh
+++ helper/alpine_dev/build.sh
@@ -3,6 +3,9 @@
 set -x
 set -e
 cd `dirname $0` && docker pull alpine 
+if [ ! -e execve_wrapper.c ]; then
+    cp ../execve_wrapper.c .
+fi
 docker build --rm --no-cache -t tal_base_alpine_dev:latest .
 docker tag tal_base_alpine_dev:latest fderepas/tal_base_alpine_dev:latest
 docker push fderepas/tal_base_alpine_dev:latest
