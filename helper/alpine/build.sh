12adc047b1cddc71b1750f2432151ecb0c673e4a
Fabrice Derepas <fabrice@derepas.com>
2024-08-08 15:07:25 +0200

    put execve_wrapper.c in just one location

--- helper/alpine/build.sh
+++ helper/alpine/build.sh
@@ -2,7 +2,10 @@
 # build the docker image
 set -x
 set -e
-cd `dirname $0` && docker pull alpine 
+cd `dirname $0` && docker pull alpine
+if [ ! -e execve_wrapper.c ]; then
+    cp ../execve_wrapper.c .
+fi
 docker build --rm --no-cache -t tal_base_alpine:latest .
 docker tag tal_base_alpine:latest fderepas/tal_base_alpine:latest
 docker push fderepas/tal_base_alpine:latest
