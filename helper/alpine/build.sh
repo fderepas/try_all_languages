56ef2513869c5bd530a9d9d33eedf033aa922346
Fabrice Derepas <fabrice.derepas@polytechnique.org>
2024-08-09 09:14:36 +0000

    test different architectures

--- helper/alpine/build.sh
+++ helper/alpine/build.sh
@@ -6,6 +6,8 @@ cd `dirname $0` && docker pull alpine
 if [ ! -e execve_wrapper.c ]; then
     cp ../execve_wrapper.c .
 fi
+#docker buildx create --use
+#docker buildx build --platform=linux/amd64,linux/arm64 --rm --no-cache -t tal_base_alpine:latest .
 docker build --rm --no-cache -t tal_base_alpine:latest .
 docker tag tal_base_alpine:latest fderepas/tal_base_alpine:latest
 docker push fderepas/tal_base_alpine:latest
