85c4f77a15269de0416c7bf92d34792b2e2ac56d
fabrice <fabrice.derepas@polytechnique.org>
2025-08-07 20:24:31 +0000

    updated dyalog version

--- docker/apl/build.sh
+++ docker/apl/build.sh
@@ -5,7 +5,7 @@ set -e
 if [ -f apl.deb ]; then
     echo using file apl.deb
 else
-    wget -O apl.deb https://www.dyalog.com/uploads/php/download.dyalog.com/download.php?file=19.0/linux_64_19.0.49960_unicode.x86_64.deb
+    wget -O apl.deb https://www.dyalog.com/uploads/php/download.dyalog.com/download.php?file=19.0/linux_64_19.0.50027_unicode.x86_64.deb
 fi
 cd `dirname $0` && docker pull alpine && docker pull ubuntu
 docker build --rm --no-cache -t tal-$(basename `pwd`):`date +"%Y%m%d"`  -t tal-$(basename `pwd`):latest .
