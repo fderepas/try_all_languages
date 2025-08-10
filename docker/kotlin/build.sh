e13f0eefacbae67912d535bf50183ab3e97cad9c
fabrice <fabrice.derepas@polytechnique.org>
2025-08-10 14:12:38 +0000

    updated Kotin with new Docker security rules.

--- docker/kotlin/build.sh
+++ docker/kotlin/build.sh
@@ -3,6 +3,8 @@
 set -x
 set -e
 cd `dirname $0` && docker pull alpine && docker pull ubuntu
-docker build --rm --no-cache -t tal-$(basename `pwd`):`date +"%Y%m%d"`  -t tal-$(basename `pwd`):latest .
+GITHUB_UCONTENT_IP=`getent ahosts "release-assets.githubusercontent.com" | awk '{print $1; exit}'`
+GITHUB_IP=`getent ahosts "github.com" | awk '{print $1; exit}'`
+docker build --rm --no-cache --add-host="release-assets.githubusercontent.com:$GITHUB_UCONTENT_IP" --add-host="github.com:$GITHUB_IP" -t tal-$(basename `pwd`):`date +"%Y%m%d"`  -t tal-$(basename `pwd`):latest .
 
 
