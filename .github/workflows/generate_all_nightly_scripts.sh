80e332724649c3e1af991bf41460bb775bb85228
Fabrice Derepas <fabrice.derepas@polytechnique.org>
2025-08-10 10:14:28 +0200

    added al nightly scripts

---
+++ .github/workflows/generate_all_nightly_scripts.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+m_counter=0
+h_counter=4
+for lang in ada apl assembly b bash bqn c clojure cobol cpp csharp dart dc elixir erlang fig fortran fsharp go golfscript groovy haskell j java jelly julia k kotlin lisp logo lua node ocaml perl php postscript powershell prolog python r raku ruby rust scala sql swift typescript vyxal zsh ; do
+    echo $lang
+    cat <<EOF > nightly_rebuild_$lang.yml
+name: $lang Nightly Rebuild
+
+on:
+  # Trigger the workflow every night at 4:00 AM UTC
+  schedule:
+    - cron: '$m_counter $h_counter * * *'
+
+  # Allows to run this workflow manually from the Actions tab
+  workflow_dispatch:
+
+jobs:
+  run-tests-on-local-machine:
+    runs-on: self-hosted
+
+    steps:
+      # Check out repository's code onto local machine
+      - name: Check out code
+        uses: actions/checkout@v4
+
+      # Testing command
+      - name: Build images
+        run: |
+          echo "Building images"
+          cd docker/$lang
+          make
+          if [ $? -eq 0 ]; then
+              make test > /dev/null 2> /dev/null
+              if [ $? -eq 0 ]; then
+                  printf \\033[32mOK\\033[0m"\n"
+                  push_to_registry $j fderepas latest
+              else
+                  printf \\033[31mtest_\KO\\033[0m"\n"
+              fi
+          else
+              printf \\033[31mKO\\033[0m"\n"
+              exit 1
+          fi
+EOF
+    m_counter=$((m_counter+1))
+    if [[ "$m_counter" -ge 60 ]]; then
+        m_counter=$((m_counter-60))
+        h_counter=$((h_counter+1))
+    fi
+done
