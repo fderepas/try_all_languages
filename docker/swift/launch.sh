c1ddc5d27bf270431af0c32ff16d13c2ff6d6454
fabrice <fabrice.derepas@polytechnique.org>
2025-09-08 16:44:02 +0000

    show output of tests.

--- docker/swift/launch.sh
+++ docker/swift/launch.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 # this script is to be run from a docker image
 countNbTest=$1
+cd 
+cat <<EOF > compile_and_exec.sh
+#!/bin/bash -l
 set +e # support errors
 cd
+. .local/share/swiftly/env.sh 
 swiftc  /mnt/in/prog.swift
 for testcount in `seq 0 1 $countNbTest`
 do
@@ -12,3 +16,6 @@ do
                  2> /mnt/out/err$testcount.txt
     echo $? > /mnt/out/errcode$testcount.txt
 done
+
+EOF
+bash compile_and_exec.sh
