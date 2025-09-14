6faf619ecabe32d065a14d7866b6385e7718ebee
fabrice <fabrice.derepas@polytechnique.org>
2025-09-14 20:14:49 +0000

    fixed version with multiline output

--- docker/update_version.sh
+++ docker/update_version.sh
@@ -3,7 +3,7 @@ cd `dirname $0`
 echo "{" > version.json
 for i in `ls -F | grep / | tr -d '/'`; do
     cd $i
-    v=`bash version.sh 2>&1 |  sed -e 's/\r//'|  sed -e 's/"/\\\\"/g'`
+    v=`bash version.sh 2>&1 |  sed -e 's/\r/ /'|  sed -z 's/\n/ /g'|  sed -e 's/"/\\\\"/g'`
     cd ..
     echo "  \"$i\":\"$v\"," >> version.json
 done
