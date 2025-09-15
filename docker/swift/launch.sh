2de500f1c7b8a7081f41e9b9d7cf47d5d5301cbc
fabrice <fabrice.derepas@polytechnique.org>
2025-09-15 19:15:46 +0000

    updated launch script

--- docker/swift/launch.sh
+++ docker/swift/launch.sh
@@ -3,19 +3,20 @@
 countNbTest=$1
 cd 
 cat <<EOF > compile_and_exec.sh
-#!/bin/bash -l
+#!/bin/bash
 set +e # support errors
+maxt=\$1
 cd
 . .local/share/swiftly/env.sh 
 swiftc  /mnt/in/prog.swift
-for testcount in `seq 0 1 $countNbTest`
+for testcount in \`seq 0 1 \$maxt\`
 do
     ./prog                                     \
-                 < /mnt/in/input$testcount.txt \
-                 > /mnt/out/out$testcount.txt  \
-                 2> /mnt/out/err$testcount.txt
-    echo $? > /mnt/out/errcode$testcount.txt
+                 < /mnt/in/input\$testcount.txt \
+                 > /mnt/out/out\$testcount.txt  \
+                 2> /mnt/out/err\$testcount.txt
+    echo \$? > /mnt/out/errcode\$testcount.txt
 done
 
 EOF
-bash compile_and_exec.sh
+bash -l compile_and_exec.sh $countNbTest
